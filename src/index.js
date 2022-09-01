a64d3ee600aece5adb8bb5a12c9bfe8110305198
mandrianova <MAndria3@its.jnj.com>
2022-09-01 22:38:59 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import _ from 'lodash';
 import './style.scss';
 import 'bootstrap';
+import html from "./index.html";
+console.log(html)
+
+document.body.innerHTML = html
+
+let title = document.querySelector('h1')
+console.log(title)
+title.innerHTML = "Lol"
+
+const results = {
+  "01": "Text",
+  "10": "Text",
+  "00": "Text"
+}
+results[score]
+
+const questions = [
+  {"question": "Text", answers: [{"answer": "Yes", value: "1"}, {}]}
+]
+questions[0]
+
+results = ["Text1", "Text2", "Text3"]
+results[score]
+results[4]
