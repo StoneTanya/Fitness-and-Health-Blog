f36a5d383e5793ce74d65be9401e74f1edb5c749
Margarita Shishkova <margolen239@gmail.com>
2022-09-04 18:58:18 +0300

    Add quiz 3

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
-import _ from 'lodash';
-import './style.scss';
-import 'bootstrap';
+import _ from "lodash";
+import "./style.scss";
+import "bootstrap";
 import html from "./index.html";
-console.log(html)
+console.log(html);
 
-document.getElementById("content").innerHTML = html
-
-let title = document.querySelector('h1')
-console.log(title)
-title.innerHTML = "Lol"
-
-const results = {
-  "01": "Text",
-  "10": "Text",
-  "00": "Text"
-}
-results[score]
-
-const questions = [
-  {"question": "Text", answers: [{"answer": "Yes", value: "1"}, {}]}
-]
-questions[0]
-
-results = ["Text1", "Text2", "Text3"]
-results[score]
-results[4]
+document.getElementById("content").innerHTML = html;
