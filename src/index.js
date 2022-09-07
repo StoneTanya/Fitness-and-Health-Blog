c040c27e310eae62ab80252c599240bf72bfa774
Margarita Shishkova <margolen239@gmail.com>
2022-09-07 20:31:44 +0300

    Quiz3

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import "./style.scss";
 import "bootstrap";
 
 import template from "./template.html";
-
-import { test3 } from "./test3";
-
 document.body.innerHTML = template;
 
+import { test3 } from "./test3";
 test3();
+
+import { quiz3 } from "./quiz3";
+quiz3();
