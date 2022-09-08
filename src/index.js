8fd0fa83fa5aceb35f204b8c606f35e008a0e248
Margarita Shishkova <margolen239@gmail.com>
2022-09-08 22:57:19 +0300

    Add quiz-2

--- src/index.js
+++ src/index.js
@@ -3,13 +3,7 @@ import "./style.scss";
 import "bootstrap";
 import template from "./template.html";
 import ComponentTestPurpose from "./components/testPurposeBlock/testPurpose";
-// import ComponentNav from "./components/header/nav";
 
 document.innerHTML = template;
-// ComponentNav();
-ComponentTestPurpose();
 
-startTest();
-console.log(results);
-ComponentTest();
-ComponentHeader();
+ComponentTestPurpose();
