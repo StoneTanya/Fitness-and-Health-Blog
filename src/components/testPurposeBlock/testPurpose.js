2cabbb6cfcaff250bb2dc204c6772038c0acbe85
Margarita Shishkova <margolen239@gmail.com>
2022-09-10 16:23:36 +0300

    Create quiz_4

--- src/components/testPurposeBlock/testPurpose.js
+++ src/components/testPurposeBlock/testPurpose.js
@@ -1,6 +1,6 @@
 import testDiv from "./testBlock_Purpose.html";
 import quiz_2 from "../quiz_1_3/quiz-2.html";
-
+import quiz_4 from "../quiz_1_3/quiz-4.html";
 import testHobby from "../quiz_1_3/quiz_wrapper.html";
 
 import { startTest1 } from "../quiz_1_3/quiz-1";
@@ -16,6 +16,7 @@ export default function ComponentTestPurpose() {
   let buttonHobby = document.getElementById("button__hobby");
   let buttonStress = document.getElementById("button__stress");
   let buttonLook = document.getElementById("button__look");
+  let buttonMedicine = document.getElementById("button__medicine");
   const quizDiv = document.getElementById("quiz__wrapper");
 
   buttonHobby.addEventListener("click", function () {
@@ -34,5 +35,10 @@ export default function ComponentTestPurpose() {
     testPurposeDiv.style.display = "none";
     quizDiv.innerHTML = testHobby;
     startTest3();
+  });
+
+  buttonMedicine.addEventListener("click", function () {
+    testPurposeDiv.style.display = "none";
+    quizDiv.innerHTML = quiz_4;
   });
 }
