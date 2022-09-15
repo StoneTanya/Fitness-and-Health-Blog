a2f868305fa5bf305753a08b3c2ab885d7baeee2
StoneTanya <kameneva.tania@mail.ru>
2022-09-15 18:15:54 +0300

    return photo

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,12 +3,11 @@ import mainPhoto from "../../photo/IMG_9600.JPG";
 
 export default function ComponentAbout() {
     const aboutDiv = document.getElementById('about');
-    const mainPhotoWrapper = document.getElementById('about__photo');
     aboutDiv.innerHTML = about;
     
     const aboutPhoto = new Image();
-    aboutPhoto.className = "col-6";
+    aboutPhoto.className = "col-md-6 col-12";
     aboutPhoto.src = mainPhoto;
-    mainPhotoWrapper.appendChild(aboutPhoto);
+    aboutDiv.appendChild(aboutPhoto);
 }
 
