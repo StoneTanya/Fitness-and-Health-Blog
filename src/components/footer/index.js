d099e6b40606805518c9c4b876761346af36afd7
StoneTanya <kameneva.tania@mail.ru>
2022-09-15 18:18:38 +0300

    delete add event listener

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,13 +1,8 @@
 import footer from "./footer.html";
-import { startPopup } from "../popup/popup";
 
 
 export default function ComponentFooter() {
     const footerDiv = document.getElementById('footer');
     footerDiv.innerHTML = footer;
-    document.addEventListener('DOMContentLoaded', (event) => {
-        event.preventDefault();
-        startPopup();
-    });
 }
 
