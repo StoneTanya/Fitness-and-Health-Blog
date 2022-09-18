ece12427f0ebc3f07dd7da153942c7af7d26b43b
Margarita Shishkova <margolen239@gmail.com>
2022-09-19 00:05:59 +0300

    Change articles

--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -4,9 +4,26 @@ import articlesDivFigure from "./articlesFigure.html";
 export function ComponentArticlesMental() {
   const articlesBlockM = document.getElementById("articles_block");
   articlesBlockM.innerHTML = articlesDivMental;
-};
+  subscribeReadMoreClick();
+}
 
 export function ComponentArticlesFigure() {
   const articlesBlockF = document.getElementById("articles_block");
   articlesBlockF.innerHTML = articlesDivFigure;
+  subscribeReadMoreClick();
+}
+
+function subscribeReadMoreClick() {
+  let readMoreBtns = document.querySelectorAll(".read_more");
+  const hiddenArticles = document.getElementsByClassName(
+    ".article__text_hidden"
+  );
+  let i = 0;
+
+  readMoreBtns.forEach(function (btn) {
+    btn.addEventListener("click", () => {
+      console.log("check:" + readMoreBtns);
+      hiddenArticles.style.color = "red";
+    });
+  });
 }
