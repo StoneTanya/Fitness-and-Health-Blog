12e86b0107b6f88a046ad894bce3b6e08fb8a290
StoneTanya <kameneva.tania@mail.ru>
2022-09-20 19:51:30 +0300

    add the desition to the Articles

--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -4,26 +4,31 @@ import articlesDivFigure from "./articlesFigure.html";
 export function ComponentArticlesMental() {
   const articlesBlockM = document.getElementById("articles_block");
   articlesBlockM.innerHTML = articlesDivMental;
-  subscribeReadMoreClick();
+  let readMoreBtns = document.getElementsByClassName("read_more"); 
+  for(let btn of readMoreBtns) 
+  btn.onclick=expandArticle;
 }
 
 export function ComponentArticlesFigure() {
   const articlesBlockF = document.getElementById("articles_block");
   articlesBlockF.innerHTML = articlesDivFigure;
-  subscribeReadMoreClick();
+  let readMoreBtns = document.getElementsByClassName("read_more"); 
+  for(let btn of readMoreBtns) 
+  btn.onclick=expandArticle;
 }
 
-function subscribeReadMoreClick() {
-  let readMoreBtns = document.querySelectorAll(".read_more");
-  const hiddenArticles = document.getElementsByClassName(
-    ".article__text_hidden"
-  );
-  let i = 0;
+function expandArticle(n){
+  let e = n.target;     //кнопка
+  e.toggleAttribute("data-collapsed");    // класс бутстрапа collapsed
+  console.log(e);
 
-  readMoreBtns.forEach(function (btn) {
-    btn.addEventListener("click", () => {
-      console.log("check:" + readMoreBtns);
-      hiddenArticles.style.color = "red";
-    });
-  });
+  let t = e.getAttribute("data-collapsed")==="", 
+  s = e.parentNode,   //родитель с классом summary
+  o = s.parentNode;   // родитель родителя
+  console.log(t);
+  console.log(s);
+  console.log(o);
+//тернарный оператор - если есть указанный класс, кнопка развернуть - и скрол, если нет - кнопка свернуть
+  o.classList.toggle("articles__content--open"), 
+  t ? (e.innerHTML="+ Развернуть", window.scrollTo(0, o.offsetTop-150)):(e.innerHTML="- Свернуть")
 }
