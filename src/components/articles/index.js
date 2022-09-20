48bfe9a6478cec9979a190a8ec584277040685e4
StoneTanya <kameneva.tania@mail.ru>
2022-09-21 01:15:39 +0300

    add styles
    Co-authored-by: Margarita <Margolen@users.noreply.github.com>

--- src/components/articles/index.js
+++ src/components/articles/index.js
@@ -4,31 +4,29 @@ import articlesDivFigure from "./articlesFigure.html";
 export function ComponentArticlesMental() {
   const articlesBlockM = document.getElementById("articles_block");
   articlesBlockM.innerHTML = articlesDivMental;
-  let readMoreBtns = document.getElementsByClassName("read_more"); 
-  for(let btn of readMoreBtns) 
-  btn.onclick=expandArticle;
+  initCollapse();
 }
 
 export function ComponentArticlesFigure() {
   const articlesBlockF = document.getElementById("articles_block");
   articlesBlockF.innerHTML = articlesDivFigure;
+  initCollapse();
+}
+
+function initCollapse() {
   let readMoreBtns = document.getElementsByClassName("read_more"); 
   for(let btn of readMoreBtns) 
   btn.onclick=expandArticle;
-}
+} 
 
 function expandArticle(n){
-  let e = n.target;     //кнопка
+  let e = n.target;   
+    //кнопка
   e.toggleAttribute("data-collapsed");    // класс бутстрапа collapsed
-  console.log(e);
 
   let t = e.getAttribute("data-collapsed")==="", 
-  s = e.parentNode,   //родитель с классом summary
-  o = s.parentNode;   // родитель родителя
-  console.log(t);
-  console.log(s);
-  console.log(o);
+  s = e.parentNode,      //родитель с классом summary
+  o = s.parentNode;      // родитель родителя
 //тернарный оператор - если есть указанный класс, кнопка развернуть - и скрол, если нет - кнопка свернуть
-  o.classList.toggle("articles__content--open"), 
-  t ? (e.innerHTML="+ Развернуть", window.scrollTo(0, o.offsetTop-150)):(e.innerHTML="- Свернуть")
+  o.classList.toggle("articles__content--open"), t ? (e.innerHTML="+ Читать далее", window.scrollTo(0, o.offsetTop-150)):(e.innerHTML="- Свернуть")
 }
