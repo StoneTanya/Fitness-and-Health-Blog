87276d73e5b25e96f87adce249fa5be5fb24dc59
StoneTanya <kameneva.tania@mail.ru>
2022-09-23 14:55:17 +0300

    add popup to sertificate and privacypolicy

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -5,17 +5,9 @@ import { SertSlider } from "../gallery/index";
 export default function ComponentNav() {
     const navDiv = document.getElementById('navigation');
     navDiv.innerHTML = nav;
-    navDiv.querySelector(".open-popup").addEventListener("click", (event) => {
+    document.querySelector(".open-popup").addEventListener("click", (event) => {
         event.preventDefault();
         SertSlider();
     })
 };
 
-// export function ComponentSliderPopup() {
-//     let openPopup = document.querySelector('.open-popup');
-
-//     openPopup.addEventListener('click', function(e) {            
-//             e.preventDefault();                  
-//             Popup();            // И для самого окна
-//         })
-// };
