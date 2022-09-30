719d7439d884a7396c2c9aa7cf4be1a306af56f2
StoneTanya <kameneva.tania@mail.ru>
2022-09-30 19:58:09 +0300

    add privacy

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -22,7 +22,27 @@ export default function ComponentFooter() {
 export function pdfViewer() {
     const pdfEl = document.getElementById("pdf__window");
     pdfEl.innerHTML = privacy;
-    // let pdf = document.querySelector(".pdf__block");            
-    // pdf.classList.add('active');
+    let pdf = document.querySelector(".pdf__block"); 
+    pdf.classList.add('active');
+    // let newWindow = open('', 'example', 'width=300,height=300')
+    // newWindow.focus();
+    
+    // alert(newWindow.location.href); // (*) about:blank, загрузка ещё не началась
+    // newWindow.onload = function() {
+    //     let privacy = `<object class="pdf__block" data="../../image/materials/PrivacyPolicy.pdf" type="application/pdf" width="768" height="1024">
+    //         <p>
+    //             Ваш браузер не поддерживает PDF-файлы.
+    //             <a href="../../image/materials/PrivacyPolicy.pdf" >Загрузить Политика конфиденциальности</a>.
+    //         </p>
+    //     </object>`;
+    //     newWindow.document.getElementById("pdf__window").insertAdjacentHTML('afterbegin', privacy);
+    // };
+
+
+//     let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+//     width=600,height=300,left=100,top=100`;
+//     let pdfWindow = window.open("", "Политика конфиденциальности", params);
+
+
 };
 
